3879137adf66905401b59283b5cce95739f59b73
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-01-13 17:01:25 +0900

    debug

--- pagerank/src/lib/pagerank.cpp
+++ pagerank/src/lib/pagerank.cpp
@@ -77,7 +77,7 @@ void Pagerank::calc_pagerank_value(int start, int end){
 void Pagerank::change_pagerank_value(){
     for(int i = 0;i<pagerank.num_of_vertex;i++){
         pagerank.pr[i] = pagerank.new_pr[i];
-        cout << "pr[" <<i<<"]: " << pagerank.pr[i] <<endl
+        cout << "pr[" <<i<<"]: " << pagerank.pr[i] <<endl;
     }
 }
 void Pagerank::combine_pr(){
