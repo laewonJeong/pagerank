dec1aacd81bfdae3ce5d5df5734bea79d20f9f0d
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-01-18 18:45:01 +0900

    230118 last commit

--- pagerank/src/lib/pagerank.cpp
+++ pagerank/src/lib/pagerank.cpp
@@ -167,7 +167,7 @@ void Pagerank::run_pagerank(int iter){
         Pagerank::send_recv_pagerank_value(pagerank.start1,pagerank.end1);
         Pagerank::combine_pr();
         //cout << diff <<endl;
-        if(diff < 0.001){
+        if(diff < 0.1){
             break;
         }
         pagerank.pr = pagerank.new_pr;
