05d1242763b12a9b00e394594bfb0fe040c7b2ef
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-01-25 18:50:11 +0900

    debug

--- pagerank/src/lib/pagerank.cpp
+++ pagerank/src/lib/pagerank.cpp
@@ -220,7 +220,7 @@ void Pagerank::run_pagerank(int iter){
         }
 
         //cout << pagerank.diff <<endl;
-        if(pagerank.diff < 0.00001 || fabs(pagerank.diff - prev_diff) <0.000001){
+        if(pagerank.diff < 0.00001 || fabs(pagerank.diff - prev_diff) <0.0000001){
             break;
         }
         prev_diff = pagerank.diff;
