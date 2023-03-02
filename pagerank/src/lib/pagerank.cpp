9d254871fe1dc18d1b1cb0766c1ff165c09ae982
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-03-02 18:03:49 +0900

    Test

--- pagerank/src/lib/pagerank.cpp
+++ pagerank/src/lib/pagerank.cpp
@@ -281,8 +281,8 @@ void Pagerank::init_connection(const char* ip, string server[], int number_of_se
     pagerank.num_of_server = number_of_server;
     pagerank.diff = 1;
     for(int i=1;i<number_of_server;i++){
-        if(ip == server[1]){
-            pagerank.start1 = pagerank.num_of_vertex/(number_of_server-1)*i;
+        if(ip == server[i]){
+            pagerank.start1 = pagerank.num_of_vertex/(number_of_server-1)*(i-1);
             pagerank.end1 = pagerank.start1 + pagerank.num_of_vertex/(number_of_server-1);
         }
         if(ip == server[number_of_server-1]){
