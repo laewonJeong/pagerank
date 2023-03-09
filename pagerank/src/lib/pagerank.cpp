576ce1af0730264a24669673c142bdd9cdfabe52
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-03-09 18:59:19 +0900

    Test

--- pagerank/src/lib/pagerank.cpp
+++ pagerank/src/lib/pagerank.cpp
@@ -10,8 +10,8 @@ myRDMA myrdma1;
 Pagerank pagerank;
 vector<int> sock_idx;
 static std::mutex mutx;
-vector<double> send_buffer[4];
-vector<double> recv_buffer[4];
+vector<double> send_buffer[3];
+vector<double> recv_buffer[3];
 int n, n1;
 
 vector<string> split(string str, char Delimiter) {
