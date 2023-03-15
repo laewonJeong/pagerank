ce958b2afd1c9da45876b48980b3ac0ffa8e8cf3
laewonjeong <laewonjeong@kangwon.ac.kr>
2023-03-15 18:07:09 +0900

    test

--- pagerank/src/network/myRDMA.cpp
+++ pagerank/src/network/myRDMA.cpp
@@ -28,12 +28,17 @@ char* change(string temp){
   return stc;
 }
 
-void myRDMA::rdma_send_pagerank(vector<double> msg, int i){
+void myRDMA::rdma_send_pagerank(vector<double> msg, int j){
     size_t size = sizeof(double)*(myrdma.num_of_vertex);
     struct ibv_wc wc;
-    rdma.post_rdma_send(rdma_info1[0][i].qp, rdma_info1[0][i].mr, send_adrs[i], 
+    
+    for(size_t i =0;i<3;i++)
+        rdma.post_rdma_send(rdma_info1[0][i].qp, rdma_info1[0][i].mr, send_adrs[i], 
                         size, myrdma.qp_key[i].first, myrdma.qp_key[i].second);
-    //rdma.pollCompletion(rdma_info1[0][i].cq);
+    
+    rdma.pollCompletion(rdma_info1[0][0].cq);
+    rdma.pollCompletion(rdma_info1[0][1].cq);
+    rdma.pollCompletion(rdma_info1[0][2].cq);
     //while(ibv_poll_cq(rdma_info1[0][i].cq,1,&wc)==0){}
 }
 void myRDMA::rdma_recv_pagerank(int i){
@@ -42,7 +47,7 @@ void myRDMA::rdma_recv_pagerank(int i){
     rdma.post_rdma_recv(rdma_info1[1][i].qp, rdma_info1[1][i].mr, 
                         rdma_info1[1][i].cq,recv_adrs[i], size);//sizeof(myrdma.recv[i].data()));
     //rdma.pollCompletion(rdma_info1[1][i].cq);
-    //while(ibv_poll_cq(rdma_info1[1][i].cq,1,&wc)==0){}
+    while(ibv_poll_cq(rdma_info1[1][i].cq,1,&wc)==0){}
     
 }
 void myRDMA::rdma_write_pagerank(vector<double> msg, int i){
